13e14c5a807fc1a45093244c34a8eb485e6357c8
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-19 10:43:03 -0600

    Add tests for movie index page

---
+++ spec/features/movies/index_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'the Movie index page' do
+  before do
+    @warner = Studio.create!(
+      name: 'Warner Bros.',
+      location: 'Hollywood'
+    )
+    @lucasfilm = Studio.create!(
+      name: 'Lucasfilm Ltd.',
+      location: 'Hollywood'
+    )
+    @disney = Studio.create!(
+      name: 'Disney',
+      location: 'Hollywood'
+    )
+    @raiders = @lucasfilm.movies.create!(
+      title: 'Raiders of the Lost Ark',
+      creation_year: 1981,
+      genre: 'Action/Adventure'
+    )
+    @temple_doom = @lucasfilm.movies.create!(
+      title: 'Indiana Jones and the Temple of Doom',
+      creation_year: 1984,
+      genre: 'Action/Adventure'
+    )
+    @last_crusade = @lucasfilm.movies.create!(
+      title: 'Indiana Jones and the Last Crusade',
+      creation_year: 1989,
+      genre: 'Action/Adventure'
+    )
+  end
+  it 'should display all movies' do
+    visit '/movies'
+
+    expect(page).to have_content(@raiders.title)
+    expect(page).to have_content(@temple_doom.title)
+    expect(page).to have_content(@last_crusade.title)
+  end
+end
