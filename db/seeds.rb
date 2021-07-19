3ed97cd28b3aa6953c4253bb9e3549726bfbeb34
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-19 10:57:29 -0600

    Make seeds to start with content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+Studio.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+MovieActor.destroy_all
+
 universal = Studio.create(name: 'Universal Studios', location: 'Hollywood')
-universal.movies.create(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+lucasfilm = Studio.create(name: 'Lucasfilm Ltd.', location: 'Hollywood')
+lucasfilm.movies.create(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
 universal.movies.create(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
